86f77fb5ccb5312c75a0e49ec999da4e4d3cb201
Gilbert Gwizdała <ggwizdala@sointeractive.pl>
2017-09-11 15:43:38 +0200

    Add empty project

---
+++ mlExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mlExample
+//
+//  Created by Gilbert Gwizdała on 11/09/2017.
+//  Copyright © 2017 Gilbert Gwizdała. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
